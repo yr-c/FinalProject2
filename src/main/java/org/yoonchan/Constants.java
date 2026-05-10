e8d7d7ee53bafab9c82279be95604af91b02abeb
yoon <yoonchan.rhie@gmail.com>
2026-05-09 22:28:55 -0400

    Added constants to Constants class.

--- src/main/java/org/yoonchan/Constants.java
+++ src/main/java/org/yoonchan/Constants.java
@@ -1,4 +1,8 @@
 package org.yoonchan;
 
 public class Constants {
+    public static final int MAX_BOOKS_STUDENT = 5;
+    public static final int MAX_ITEMS_TEACHER = 10;
+    public static final String BOOKS_CSV_PATH = "src/main/resources/books.csv";
+    public static final String USERS_CSV_PATH = "src/main/resources/users.csv";
 }
