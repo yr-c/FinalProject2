6f272bbae3f7eac6d409ba3591508df04b6f304d
yoon <yoonchan.rhie@gmail.com>
2026-05-12 10:05:03 -0400

    Made Genre an inner class of Book and finished Book boilerplate

--- src/main/java/org/yoonchan/Book.java
+++ src/main/java/org/yoonchan/Book.java
@@ -1,14 +1,35 @@
 package org.yoonchan;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = false)
+@Getter
+@ToString
 public class Book extends Item {
     private static int nextId = 1;
+    @Setter private String ISBN;
+    @Setter private String author;
+    @Setter private Genre genre;
 
     // For CSV functionality
-    public Book(String id, String title, Status status) {
+    public Book(String id, String title, Status status, String ISBN, String author, Genre genre) {
         super(id, title, status);
+        this.ISBN = ISBN;
+        this.author = author;
+        this.genre = genre;
     }
 
-    public Book(String title, Status status) {
-        super(String.format("B%04d", nextId++), title, status);
+    public Book(String title, String ISBN, String author, Genre genre) {
+        super(String.format("B%04d", nextId++), title);
+        this.ISBN = ISBN;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public enum Genre {
+        FANTASY, SCI_FI, HORROR, NON_FICTION
     }
 }
