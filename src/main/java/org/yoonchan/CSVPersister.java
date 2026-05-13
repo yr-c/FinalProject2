8793f332f8de543c4645492bd9fcda8bf41ab935
yoon <yoonchan.rhie@gmail.com>
2026-05-13 16:23:07 -0400

    Implemented loadItemsFromCSV method and fixed malformed data entries created by save methods

--- src/main/java/org/yoonchan/CSVPersister.java
+++ src/main/java/org/yoonchan/CSVPersister.java
@@ -5,9 +5,11 @@ import org.yoonchan.roles.*;
 import org.yoonchan.util.Constants;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public interface CSVPersister {
     /**
@@ -15,7 +17,7 @@ public interface CSVPersister {
      * the CSV files {@code items.csv} and {@code users.csv}. Invokes both
      * loadItemsFromCSV and loadUsersFromCSV methods.
      */
-    default void loadAllFromCSV() {
+    default void loadAllFromCSV(){
         loadItemsFromCSV();
         loadUsersFromCSV();
     }
@@ -26,8 +28,61 @@ public interface CSVPersister {
      */
     default void loadItemsFromCSV() {
         File file = new File(Constants.ITEMS_CSV_PATH);
+        int bookNum = 0;
+        int dvdNum = 0;
+        int magazineNum = 0;
 
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] elements = line.split(",");
 
+                String id = elements[0];
+                String title = elements[1];
+                Item.Status status = Item.Status.valueOf(elements[2]);
+
+                switch (elements[3]) {
+                    case "Book" -> {
+                        String isbn = elements[4];
+                        String author = elements[5];
+                        String genre = elements[6];
+
+                        Item item = new Book(id, title, status, isbn, author, genre);
+                        Library.itemCatalogue.add(item);
+                        bookNum++;
+                    }
+
+                    case "DVD" -> {
+                        String publisher = elements[4];
+                        int issueNumber = Integer.parseInt(elements[5]);
+
+                        Item item = new DVD(id, title, status, publisher, issueNumber);
+                        Library.itemCatalogue.add(item);
+                        dvdNum++;
+                    }
+
+                    case "Magazine" -> {
+                        String director = elements[4];
+                        int durationMins = Integer.parseInt(elements[5]);
+
+                        Item item = new Magazine(id, title, status, director, durationMins);
+                        Library.itemCatalogue.add(item);
+                        dvdNum++;
+                    }
+
+                    default -> {
+                        System.out.println("CRITICAL: Malformed data at line: " + line + "\n(Missing Item type)");
+                        throw new RuntimeException();
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        }
+
+        Book.setNextId(bookNum + 1);
+        DVD.setNextId(dvdNum + 1);
+        Magazine.setNextId(magazineNum + 1);
     }
 
     /**
@@ -36,7 +91,6 @@ public interface CSVPersister {
      */
     default void loadUsersFromCSV() {
         File file = new File(Constants.USERS_CSV_PATH);
-
 
     }
 
@@ -69,7 +123,7 @@ public interface CSVPersister {
                         data[3] = "Book";
                         data[4] = book.getISBN();
                         data[5] = book.getAuthor();
-                        data[6] = book.getGenre().toString();
+                        data[6] = book.getGenre();
                     }
 
                     case DVD dvd -> {
@@ -89,7 +143,7 @@ public interface CSVPersister {
                     default -> Arrays.fill(data, "");
                 }
 
-                fileWriter.write(String.format("%s, %s, %s, %s, %s, %s, %s\n",
+                fileWriter.write(String.format("%s,%s,%s,%s,%s,%s,%s\n",
                         data[0], data[1], data[2], data[3], data[4], data[5], data[6]));
             }
         } catch (IOException e) {
@@ -104,7 +158,7 @@ public interface CSVPersister {
     default void saveUsersToCSV() {
         File file = new File(Constants.USERS_CSV_PATH);
 
-        try (FileWriter fileWriter = new FileWriter(file)) {
+        try (FileWriter fileWriter = new FileWriter(file, false)) {
             for (User user : Library.registeredUsers) {
                 String[] data = new String[4];
                 data[0] = user.getId();
@@ -127,7 +181,7 @@ public interface CSVPersister {
                     default -> Arrays.fill(data, "");
                 }
 
-                fileWriter.write(String.format("%s, %s, %s, %s\n",
+                fileWriter.write(String.format("%s,%s,%s,%s\n",
                         data[0], data[1], data[2], data[3]));
             }
         } catch (IOException e) {
