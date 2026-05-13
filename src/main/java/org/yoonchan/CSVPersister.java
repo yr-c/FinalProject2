8e3c9f56a0ef8cf1ab051bc08089ecb3c6b71b6e
yoon <yoonchan.rhie@gmail.com>
2026-05-13 15:32:36 -0400

    Created save object methods

--- src/main/java/org/yoonchan/CSVPersister.java
+++ src/main/java/org/yoonchan/CSVPersister.java
@@ -1,6 +1,13 @@
 package org.yoonchan;
 
+import org.yoonchan.entities.*;
+import org.yoonchan.roles.*;
+import org.yoonchan.util.Constants;
+
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 
 public interface CSVPersister {
     /**
@@ -19,6 +26,7 @@ public interface CSVPersister {
      */
     default void loadItemsFromCSV() {
         File file = new File(Constants.ITEMS_CSV_PATH);
+
 
     }
 
@@ -49,6 +57,44 @@ public interface CSVPersister {
     default void saveItemsToCSV() {
         File file = new File(Constants.ITEMS_CSV_PATH);
 
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            for (Item item : Library.itemCatalogue) {
+                String[] data = new String[7];
+                data[0] = item.getId();
+                data[1] = item.getTitle();
+                data[2] = item.getStatus().toString();
+
+                switch (item) {
+                    case Book book -> {
+                        data[3] = "Book";
+                        data[4] = book.getISBN();
+                        data[5] = book.getAuthor();
+                        data[6] = book.getGenre().toString();
+                    }
+
+                    case DVD dvd -> {
+                        data[3] = "DVD";
+                        data[4] = dvd.getPublisher();
+                        data[5] = dvd.getIssueNumber() + "";
+                        data[6] = "";
+                    }
+
+                    case Magazine magazine -> {
+                        data[3] = "Magazine";
+                        data[4] = magazine.getDirector();
+                        data[5] = magazine.getDurationMins() + "";
+                        data[6] = "";
+                    }
+
+                    default -> Arrays.fill(data, "");
+                }
+
+                fileWriter.write(String.format("%s, %s, %s, %s, %s, %s, %s\n",
+                        data[0], data[1], data[2], data[3], data[4], data[5], data[6]));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 
     /**
@@ -58,5 +104,34 @@ public interface CSVPersister {
     default void saveUsersToCSV() {
         File file = new File(Constants.USERS_CSV_PATH);
 
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            for (User user : Library.registeredUsers) {
+                String[] data = new String[4];
+                data[0] = user.getId();
+                data[1] = user.getName();
+                data[2] = user.getBorrowedItems().toString();
+
+                switch (user) {
+                    case Admin admin -> {
+                        data[3] = "Admin";
+                    }
+
+                    case Student student -> {
+                        data[3] = "Student";
+                    }
+
+                    case Teacher teacher -> {
+                        data[3] = "Teacher";
+                    }
+
+                    default -> Arrays.fill(data, "");
+                }
+
+                fileWriter.write(String.format("%s, %s, %s, %s\n",
+                        data[0], data[1], data[2], data[3]));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 }
