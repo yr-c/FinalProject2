358bf73d19085f28626f31dde511fa3a33072101
yoon <yoonchan.rhie@gmail.com>
2026-05-13 15:01:34 -0400

    Created new registeredUsers list.

--- src/main/java/org/yoonchan/Library.java
+++ src/main/java/org/yoonchan/Library.java
@@ -1,7 +1,11 @@
 package org.yoonchan;
 
+import org.yoonchan.entities.Item;
+import org.yoonchan.roles.User;
+
 import java.util.List;
 
 public class Library {
     public static List<Item> itemCatalogue;
+    public static List<User> registeredUsers;
 }
