bdaf5514449b3544c06ad296211bd63f8035ca4f
yoon <yoonchan.rhie@gmail.com>
2026-05-13 13:55:32 -0400

    Implemented isISBNValid method with unit testing.

--- src/main/java/org/yoonchan/ItemUtil.java
+++ src/main/java/org/yoonchan/ItemUtil.java
@@ -1,7 +1,53 @@
 package org.yoonchan;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemUtil {
-    public static boolean isISBNValid(String ISBN) {
-        return false;
+    /**
+     * Checks whether an input code String conforms to the ISBN-13 standard (with possible hyphens included in the input).
+     * This performs a length check and verifies the check digit of the code.
+     * <p>
+     * This check digit must range from 0 to 9 and must be such that the sum of all the thirteen digits, each multiplied
+     * by its integer weight, alternating between 1 and 3, is a multiple of 10. Additionally, the code must begin with
+     * the digits 978 or 979, and contain exactly 13 digits.
+     * @param code The input code String.
+     * @return Whether the ISBN is valid.
+     */
+    public static boolean isISBNValid(String code) {
+        if (code == null) throw new NullPointerException("Input ISBN code is null.");
+
+        int standardCodeLenWithoutDashes = 13;
+        if (code.length() < standardCodeLenWithoutDashes) return false;
+
+        if (!(code.startsWith("978") || code.startsWith("979"))) return false;
+
+        List<Integer> digits = new ArrayList<>(13);
+        for (int i = 0; i < code.length(); i++) {
+            char c = code.charAt(i);
+            if (Character.isDigit(c)) {
+                digits.add(Character.getNumericValue(c));
+            }
+        }
+
+        if (digits.size() != 13) return false; // Early exit; there must be 13 digits without hyphens.
+
+        int sum = 0;
+        for (int i = 0; i < digits.size() - 1; i++) {
+            int digit = digits.get(i);
+
+            if (i % 2 == 0) {
+                sum += digit;
+            } else {
+                sum += digit * 3;
+            }
+        }
+
+        sum %= 10;
+
+        int expectedCheckDigit = 10 - sum;
+        int givenCheckDigit = digits.getLast();
+
+        return expectedCheckDigit == givenCheckDigit;
     }
 }
---
+++ src/test/java/ItemUtilTest.java
@@ -0,0 +1,63 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.yoonchan.ItemUtil;
+
+public class ItemUtilTest {
+    @Test
+    @DisplayName("testIsISBNValid1: null -> NullPointerException")
+    void testIsISBNValid1() {
+        Assertions.assertThrows(NullPointerException.class, () ->
+                ItemUtil.isISBNValid(null));
+    }
+
+    @Test
+    @DisplayName("testIsISBNValid2: \"\" -> false")
+    void testIsISBNValid2() {
+        String input = "";
+        boolean expected = false;
+        boolean actual = ItemUtil.isISBNValid(input);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("testIsISBNValid3: \"978-0-306-40615-7\" -> true")
+    void testIsISBNValid3() {
+        String input = "978-0-306-40615-7"; // Wikipedia example
+        boolean expected = true;
+        boolean actual = ItemUtil.isISBNValid(input);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("testIsISBNValid4: \"978-0553418026\" -> true")
+    void testIsISBNValid4() {
+        String input = "978-0553418026"; // The Martian
+        boolean expected = true;
+        boolean actual = ItemUtil.isISBNValid(input);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("testIsISBNValid5: \"978-0451524935\" -> true")
+    void testIsISBNValid5() {
+        String input = "978-0451524935"; // 1984
+        boolean expected = true;
+        boolean actual = ItemUtil.isISBNValid(input);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("testIsISBNValid5: \"978-0451524934\" -> false")
+    void testIsISBNValid6() {
+        String input = "978-0451524934"; // Fake - one off from 1984
+        boolean expected = false;
+        boolean actual = ItemUtil.isISBNValid(input);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
