a7736329b5b42d73c5b9b5253f7ff1dc9ab7db33
yoon <yoonchan.rhie@gmail.com>
2026-05-13 15:33:32 -0400

    Library's itemCatalogue and registeredUsers initialized as empty ArrayLists

--- src/main/java/org/yoonchan/Library.java
+++ src/main/java/org/yoonchan/Library.java
@@ -3,9 +3,10 @@ package org.yoonchan;
 import org.yoonchan.entities.Item;
 import org.yoonchan.roles.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    public static List<Item> itemCatalogue;
-    public static List<User> registeredUsers;
+    public static List<Item> itemCatalogue = new ArrayList<>();
+    public static List<User> registeredUsers = new ArrayList<>();
 }
