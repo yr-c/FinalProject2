083235758d0cb64f2da5f3aff6cf50f4b9ae47da
yoon <yoonchan.rhie@gmail.com>
2026-05-13 16:00:37 -0400

    Simplified Genre enum to simply be a String.

--- src/main/java/org/yoonchan/entities/Book.java
+++ src/main/java/org/yoonchan/entities/Book.java
@@ -12,24 +12,20 @@ public class Book extends Item {
     private static int nextId = 1;
     @Setter private String ISBN;
     @Setter private String author;
-    @Setter private Genre genre;
+    @Setter private String genre;
 
     // For CSV functionality
-    public Book(String id, String title, Status status, String ISBN, String author, Genre genre) {
+    public Book(String id, String title, Status status, String ISBN, String author, String genre) {
         super(id, title, status);
         this.ISBN = ISBN;
         this.author = author;
         this.genre = genre;
     }
 
-    public Book(String title, String ISBN, String author, Genre genre) {
+    public Book(String title, String ISBN, String author, String genre) {
         super(String.format("B%04d", nextId++), title);
         this.ISBN = ISBN;
         this.author = author;
         this.genre = genre;
-    }
-
-    public enum Genre {
-        FANTASY, SCI_FI, HORROR, NON_FICTION
     }
 }
