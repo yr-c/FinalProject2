3b531a262347362b87cc8e8363e82d70a0f474ea
yoon <yoonchan.rhie@gmail.com>
2026-05-14 14:36:41 -0400

    Created ItemRegistrar and UserRegistrar interfaces with register methods

---
+++ src/main/java/org/yoonchan/roles/ItemRegistrar.java
@@ -0,0 +1,17 @@
+package org.yoonchan.roles;
+
+import org.yoonchan.Library;
+import org.yoonchan.entities.Item;
+
+public interface ItemRegistrar {
+    /**
+     * Adds an Item to the Library's itemCatalogue.
+     * @param item The Item to be added.
+     * @throws NullPointerException If the input Item is null.
+     */
+    default void registerItemToLibrary(Item item) throws NullPointerException {
+        if (item == null) throw new NullPointerException();
+
+        Library.itemCatalogue.add(item);
+    }
+}
--- src/main/java/org/yoonchan/roles/UserRegistrar.java
+++ src/main/java/org/yoonchan/roles/UserRegistrar.java
@@ -1,4 +1,16 @@
-package org.yoonchan;
+package org.yoonchan.roles;
+
+import org.yoonchan.Library;
 
 public interface UserRegistrar {
+    /**
+     * Adds a User to the Library's registeredUsers list.
+     * @param user The User to be added.
+     * @throws NullPointerException If the input User is null.
+     */
+    default void registerUserToLibrary(User user) throws NullPointerException {
+        if (user == null) throw new NullPointerException();
+
+        Library.registeredUsers.add(user);
+    }
 }
