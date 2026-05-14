e485b1f1d91949596a97d1ada5e804c263386ab3
yoon <yoonchan.rhie@gmail.com>
2026-05-14 14:14:10 -0400

    Added testing for comparators.

--- src/test/java/ItemComparatorTest.java
+++ src/test/java/ItemComparatorTest.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.yoonchan.entities.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemComparatorTest {
+    @Test
+    @DisplayName("""
+    testItemIdComparator1:\s
+    item1 = Book("B01", "Martian", Item.Status.AVAILABLE,"978-0553418026", "Andy Weir", "Sci-Fi")\
+    item2 = Book("B02", "Martian", Item.Status.AVAILABLE,"978-0553418026", "Andy Weir", "Sci-Fi")\
+    item3 = DVD("D01", "Martian Movie", Item.Status.LOST,"Ridley Scott", 150)\
+    item4 = Magazine("M01", "Martian", Item.Status.BORROWED,"Vogue", 1)\
+    ->\
+    [item1, item4, item3, item2]""")
+    void testItemIdComparator1() {
+        List<Item> items = new ArrayList<>();
+        Item item1 = new Book("B01", "Martian", Item.Status.AVAILABLE,"978-0553418026", "Andy Weir", "Sci-Fi");
+        Item item2 = new Book("B02", "Martian", Item.Status.AVAILABLE,"978-0553418026", "Andy Weir", "Sci-Fi");
+        Item item3 = new DVD("D01", "Martian Movie", Item.Status.LOST,"Ridley Scott", 150);
+        Item item4 = new Magazine("M01", "Martian", Item.Status.BORROWED,"Vogue", 1);
+
+        items.add(item3);
+        items.add(item4);
+        items.add(item2);
+        items.add(item1);
+        items.sort(new Item.ItemIdComparator());
+
+        Object[] actual = items.toArray();
+        Object[] expected = new Object[]{item1, item4, item3, item2};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("""
+    testItemTitleComparator1:\s
+    item1 = Book("B01", "Martian", Item.Status.LOST,"978-0553418026", "Andy Weir", "Sci-Fi")\
+    item2 = Book("B02", "Martian", Item.Status.AVAILABLE,"978-0553418026", "Andy Weir", "Sci-Fi")\
+    item3 = DVD("D01", "Martian Movie", Item.Status.LOST,"Ridley Scott", 150)\
+    item4 = Magazine("M01", "Martian", Item.Status.BORROWED,"Vogue", 1)\
+    ->\
+    [item1, item4, item3, item2]""")
+    void testItemTitleComparator1() {
+        List<Item> items = new ArrayList<>();
+        Item item1 = new Book("B01", "Martian", Item.Status.LOST,"978-0553418026", "Andy Weir", "Sci-Fi");
+        Item item2 = new Book("B02", "Martian", Item.Status.AVAILABLE,"978-0553418026", "Andy Weir", "Sci-Fi");
+        Item item3 = new DVD("D01", "Martian Movie", Item.Status.LOST,"Ridley Scott", 150);
+        Item item4 = new Magazine("M01", "Martian", Item.Status.BORROWED,"Vogue", 1);
+
+        items.add(item3);
+        items.add(item1);
+        items.add(item2);
+        items.add(item4);
+        items.sort(new Item.ItemTitleComparator());
+
+        Object[] actual = items.toArray();
+        Object[] expected = new Object[]{item2, item4, item1, item3};
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
--- src/test/java/UserComparatorTest.java
+++ src/test/java/UserComparatorTest.java
@@ -1,2 +1,80 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.yoonchan.entities.Book;
+import org.yoonchan.roles.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserComparatorTest {
+    @Test
+    @DisplayName("""
+            testUserIdComparator1:\s
+            user1 = Student("01", "John", [])\
+            user2 = Teacher("02", "Adam", []);\
+            user3 = Admin("03", "Sarah", [])\
+            ->\
+            [user1, user2, user3]""")
+    void testUserIdComparator1() {
+        List<User> users = new ArrayList<>();
+        User user1 = new Student("S01", "John", new ArrayList<>());
+        User user2 = new Teacher("T02", "Adam", new ArrayList<>());
+        User user3 = new Admin("A03", "Sarah", new ArrayList<>());
+        users.add(user2);
+        users.add(user3);
+        users.add(user1);
+        users.sort(new User.UserIdComparator());
+
+        Object[] actual = users.toArray();
+        Object[] expected = new Object[]{user1, user2, user3};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("""
+            testUserNameComparator1:\s
+            user1 = Student("S01", "John", [])\
+            user2 = Student("S02", "Adam", []);\
+            user3 = Student("S03", "Sarah", [])\
+            ->\
+            [user2, user1, user3]""")
+    void testUserNameComparator1() {
+        List<User> users = new ArrayList<>();
+        User user1 = new Student("01", "John", new ArrayList<>());
+        User user2 = new Student("02", "Adam", new ArrayList<>());
+        User user3 = new Student("03", "Sarah", new ArrayList<>());
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.sort(new User.UserNameComparator());
+
+        Object[] actual = users.toArray();
+        Object[] expected = new Object[]{user2, user1, user3};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("""
+            testUserNameComparator2:\s
+            user1 = Student("S01", "Adam", [])\
+            user2 = Teacher("T02", "Adam", []);\
+            user3 = Admin("A03", "Sarah", [])\
+            ->\
+            [user2, user1, user3]""")
+    void testUserNameComparator2() {
+        List<User> users = new ArrayList<>();
+        User user1 = new Student("01", "Adam",
+                    List.of(new Book("Martian", "978-0553418026", "Andy Weir", "Sci-Fi")));
+        User user2 = new Teacher("02", "Adam", new ArrayList<>());
+        User user3 = new Admin("03", "Sarah", new ArrayList<>());
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.sort(new User.UserNameComparator());
+
+        Object[] actual = users.toArray();
+        Object[] expected = new Object[]{user2, user1, user3};
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
