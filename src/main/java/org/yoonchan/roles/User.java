a1494a3d236179078a5be1f08c2eaedfa4b5132e
yoon <yoonchan.rhie@gmail.com>
2026-05-13 23:39:13 -0400

    Implemented borrowItem method to User class

--- src/main/java/org/yoonchan/roles/User.java
+++ src/main/java/org/yoonchan/roles/User.java
@@ -1,7 +1,9 @@
 package org.yoonchan.roles;
 
 import lombok.*;
+import org.yoonchan.Library;
 import org.yoonchan.entities.Item;
+import org.yoonchan.util.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,12 +24,42 @@ public abstract class User {
     }
 
     /**
-     * Adds an item to the User's borrowedItems list. Removes that same item from the Library's
-     * itemCatalogue.
-     * @param item The item to be borrowed.
+     * Fetches the maximum Item borrow amount of a User subclass.
+     * @return The maximum Item borrow amount of the invoked subclass.
      */
-    public void borrowItem(Item item) {
+    public abstract int getMaximumBorrowAmount();
 
+    /**
+     * Adds an item to the User's borrowedItems list.
+     * @param item The item to be borrowed.
+     * @throws InvalidItemException If the invoked Item to borrow is null or not in the Library's itemCatalogue.
+     * @throws MaximumBorrowsReachedException If the User has reached his maximum borrows.
+     */
+    public void borrowItem(Item item) throws InvalidItemException, MaximumBorrowsReachedException {
+        if (item == null) {
+            throw new InvalidItemException("Invalid item. Input is null.");
+        }
+
+        if (!Library.itemCatalogue.contains(item)) {
+            throw new InvalidItemException("Invalid item. Item not found in Library's itemCatalogue.");
+        }
+
+        if (this.getBorrowedItems().size() > this.getMaximumBorrowAmount()) {
+            throw new MaximumBorrowsReachedException(
+                    String.format("Max items of %d reached for userId %s.", Constants.MAX_ITEMS_STUDENT, this.getId()));
+        }
+
+        switch (item.getStatus()) {
+            case AVAILABLE -> {
+                this.getBorrowedItems().add(item);
+                item.setStatus(Item.Status.BORROWED);
+            }
+            case BORROWED -> {
+                throw new InvalidItemException("Invalid item. Item already borrowed.");
+            }
+            case LOST ->
+                throw new InvalidItemException("Invalid item. Item lost.");
+        }
     }
 
     /**
