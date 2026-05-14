14fc32d97bd0aa211bfd9e45710ffbd6e558d31e
yoon <yoonchan.rhie@gmail.com>
2026-05-13 20:21:46 -0400

    Refactored MAX_BOOKS_STUDENT -> MAX_ITEMS_STUDENT

--- src/main/java/org/yoonchan/util/Constants.java
+++ src/main/java/org/yoonchan/util/Constants.java
@@ -1,7 +1,7 @@
 package org.yoonchan.util;
 
 public class Constants {
-    public static final int MAX_BOOKS_STUDENT = 5;
+    public static final int MAX_ITEMS_STUDENT = 5;
     public static final int MAX_ITEMS_TEACHER = 10;
     public static final String ITEMS_CSV_PATH = "src/main/resources/items.csv";
     public static final String USERS_CSV_PATH = "src/main/resources/users.csv";
