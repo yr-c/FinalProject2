9417ef839fb32cf4804ce2f8a9618cc8927855ac
yoon <yoonchan.rhie@gmail.com>
2026-05-14 16:04:53 -0400

    Implemented Reporter interface

--- src/main/java/org/yoonchan/roles/Reporter.java
+++ src/main/java/org/yoonchan/roles/Reporter.java
@@ -1,11 +1,28 @@
 package org.yoonchan.roles;
 
+import org.yoonchan.Library;
+import org.yoonchan.entities.Item;
+
 public interface Reporter {
     /**
-     * Generates a report of all borrowed/in-store/lost Items from the Library's itemCatalogue.
+     * Generates a report of all available/borrowed/lost Items from the Library's itemCatalogue.
      * Prints out a formatted list of each item, its type, and its status.
      */
     default void generateReport() {
-        System.out.println();
+        StringBuilder sbAvailableItems = new StringBuilder();
+        StringBuilder sbBorrowedItems = new StringBuilder();
+        StringBuilder sbLostItems = new StringBuilder();
+
+        for (Item item : Library.itemCatalogue) {
+            switch (item.getStatus()) {
+                case AVAILABLE -> sbAvailableItems.append("- ").append(item).append("\n");
+                case BORROWED -> sbBorrowedItems.append("- ").append(item).append("\n");
+                case LOST -> sbLostItems.append("- ").append(item).append("\n");
+            }
+        }
+
+        System.out.printf("Available items:\n%s", sbAvailableItems);
+        System.out.printf("Borrowed items:\n%s", sbBorrowedItems);
+        System.out.printf("Lost items:\n%s", sbLostItems);
     }
 }
